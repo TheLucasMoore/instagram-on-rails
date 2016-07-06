c8ef8dacf94aa9f1097f78cf6d6b51abcbf6b145
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-07-06 07:45:15 -0400

    Jquery comments

--- InstaRails/app/controllers/comments_controller.rb
+++ InstaRails/app/controllers/comments_controller.rb
@@ -7,8 +7,10 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
       if @comment.save
-        flash[:success] = "Comment successfully posted"
-        redirect_to :back
+        respond_to do |format|
+          format.html { redirect_to root_path }
+          format.js
+        end
       else
         flash[:alert] = "Something went wrong with your comment."
         redirect_to root_path
